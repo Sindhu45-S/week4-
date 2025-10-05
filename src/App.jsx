01743ba77ace13fa1451ad4c624630d6f43cf1e1
Sindhu S <sindhusugumar80@gmail.com>
2025-10-05 12:33:21 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import "./App.css";
+import { Header, Footer } from "./components";
+import Allroute from "./Routes/Allroute";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Allroute />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
